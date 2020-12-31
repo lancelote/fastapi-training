14b0cc83ee527056000b160b02ce299e97e00733
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-12-31 22:19:42 +0300

    Return JSON on error

--- src/ch03-first-api/main.py
+++ src/ch03-first-api/main.py
@@ -10,7 +10,9 @@ api = fastapi.FastAPI()
 def calculate(x: float, y: float, z: Optional[float] = None):
     if z == 0:
         return fastapi.Response(
-            content="error: z cannot be zero", status_code=400
+            content='{"error": "z cannot be zero"}',
+            status_code=400,
+            media_type="application/json",
         )
 
     value = x + y
