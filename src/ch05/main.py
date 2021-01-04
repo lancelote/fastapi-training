50ea0e815c5d1aa95c2a716f05004ee9aa57d9b0
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-04 22:19:00 +0300

    Add templates

--- src/ch05/main.py
+++ src/ch05/main.py
@@ -1,12 +1,18 @@
 import fastapi
 import uvicorn
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates("templates")
+
+api.mount("/src/ch05/static/", StaticFiles(directory="static"), name="static")
 
 
 @api.get("/")
-def index():
-    return "hello weather app"
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
