c1facce15101b9486fac1851bfbf81f73fea7c66
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-05 23:01:18 +0300

    Restructure configuration

--- src/ch05/main.py
+++ src/ch05/main.py
@@ -5,9 +5,24 @@ from starlette.staticfiles import StaticFiles
 from views import home
 
 api = fastapi.FastAPI()
-api.mount("/static", StaticFiles(directory="static"), name="static")
-api.include_router(weather.router)
-api.include_router(home.router)
+
+
+def configure_routing():
+    api.include_router(weather.router)
+    api.include_router(home.router)
+
+
+def configure_static():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def configure():
+    configure_routing()
+    configure_static()
+
 
 if __name__ == "__main__":
+    configure()
     uvicorn.run(api, host="127.0.0.1", port=8000)
+else:
+    configure()
