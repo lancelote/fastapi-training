e23ac1def46bf71f7f29d098ef5dcb90b828e8d6
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-05 22:50:47 +0300

    Restructure views

--- src/ch05/main.py
+++ src/ch05/main.py
@@ -1,29 +1,13 @@
 import fastapi
 import uvicorn
-from starlette.requests import Request
+from api import weather
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
+from views import home
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates("templates")
-
-api.mount("/src/ch05/static/", StaticFiles(directory="static"), name="static")
-
-
-@api.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@api.get("/favicon.ico")
-def favicon():
-    return fastapi.responses.RedirectResponse(url="/static/img/favicon.ico")
-
-
-@api.get("/api/weather")
-def weather():
-    return "some report"
-
+api.mount("/static", StaticFiles(directory="static"), name="static")
+api.include_router(weather.router)
+api.include_router(home.router)
 
 if __name__ == "__main__":
     uvicorn.run(api, host="127.0.0.1", port=8000)
