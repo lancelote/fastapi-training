97546c9497d06b2cbe297172f2abcfd49652d807
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-05 23:02:45 +0300

    Move templates

--- src/ch05/views/home.py
+++ src/ch05/views/home.py
@@ -8,7 +8,7 @@ templates = Jinja2Templates("templates")
 
 @router.get("/")
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 
 @router.get("/favicon.ico")
