9b587f7e19e62383734ffcb2b7c64a892168c782
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-07-22 23:24:58 +0300

    Make a request with requests

--- src/ch05/services/openweather.py
+++ src/ch05/services/openweather.py
@@ -1,8 +1,19 @@
+import requests
+
 api_key: str | None = None
 
 
 def get_report(city: str, state: str | None, country: str, units: str) -> dict:
     base_url = "https://api.openweathermap.org/data/2.5/weather"
-    query = f"{city},{country}"
+    if state:
+        query = f"{city},{state},{country}"
+    else:
+        query = f"{city},{country}"
     url = f"{base_url}?q={query}&appid={api_key}&units={units}"
-    print(url)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    data = response.json()
+    forecast = data["main"]
+    return forecast
