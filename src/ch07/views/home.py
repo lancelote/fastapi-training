8a91abc8f791a2da94a7a844a5cb183aa5e0f7a5
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-08-18 22:38:11 +0300

    Show recent events if any

--- src/ch07/views/home.py
+++ src/ch07/views/home.py
@@ -2,13 +2,17 @@ import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from services import reports
+
 router = fastapi.APIRouter()
 templates = Jinja2Templates("templates")
 
 
 @router.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request})
+async def index(request: Request):
+    events = await reports.get_reports()
+    data = {"request": request, "events": events}
+    return templates.TemplateResponse("home/index.html", data)
 
 
 @router.get("/favicon.ico")
